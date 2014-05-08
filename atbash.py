3228a9a063298dd1ecbcaa3a400f7ff91c669330
Andrew Mattheisen <andrew.j-ctr.mattheisen@faa.gov>
2014-05-08 10:50:22 -0500

    add atbash algorithm, update style of caesarian

---
+++ atbash.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+"""Atbash.
+
+Description:
+  This script performs the Atbash operation to a line of text.
+
+Author: 
+  Andrew Mattheisen
+
+Usage:
+  atbash.py <cyphertext>...
+  atbash.py (-h | --help)
+  atbash.py --version
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+
+"""
+from __future__ import print_function
+from docopt import docopt
+import string
+VERSION='1.0'
+DATE='2014-05-08'
+TITLE='Altbash'
+
+
+def main(args):
+    (cypher_text, clear_text) = altbash_encode(args['<cyphertext>'])
+    print("INPUT:  ", cypher_text)
+    print("ALTBASH:  ", clear_text)
+    return
+
+
+def altbash_encode(words):
+    clear_text = ""
+    cypher_text = ""
+    for cypher in words:
+        cypher_text += cypher
+        cypher = cypher.upper()
+        for c in cypher:
+            if not c in string.ascii_letters:
+                clear_text += c
+                continue
+            # A = 65, Z = 90
+            new_c = chr( (90 - (ord(c) - 65)) )
+            #print(c, ' -> ', new_c)
+            clear_text = ''.join([clear_text, new_c])
+        clear_text += ' '
+        cypher_text += ' '
+    return (cypher_text, clear_text)
+
+
+if __name__ == '__main__':
+    args = docopt(__doc__, version='%s %s:%s'%(TITLE, VERSION, DATE))
+    main(args)
--- caesarian.py
+++ caesarian.py
@@ -21,24 +21,25 @@ Notes:
   Used to solve http://decodeingress.me/category/code-breaking-101/
 
 """
+from __future__ import print_function
 from docopt import docopt
 import string
 VERSION='1.0'
 DATE='2014-05-06'
+TITLE='Caesarian'
 
-
-def main(args):
+def caesarian(args):
     if args['--rot'] == 0:
         for rot in range(1,26):
             (cypher_text, clear_text) = rot_encode(args['<cyphertext>'], rot)
             if rot == 1:
-                print "INPUT:  ", cypher_text
-            print "ROT%02d:  "%rot, clear_text
+                print("INPUT:  ", cypher_text)
+            print("ROT%02d:  "%rot, clear_text)
             
     else:
         (cypher_text, clear_text) = rot_encode(args['<cyphertext>'], args['--rot'])
-        print "INPUT:  ", cypher_text
-        print "ROT%2d:  "%args['--rot'], clear_text
+        print("INPUT:  ", cypher_text)
+        print("ROT%2d:  "%args['--rot'], clear_text)
     return
 
 
@@ -53,7 +54,7 @@ def rot_encode(words, rot):
                 clear_text += c
                 continue
             new_c = chr((ord(c) - 65 + rot ) % 26 + 65 )
-            #print c, ' -> ', new_c
+            #print(c, ' -> ', new_c)
             clear_text = ''.join([clear_text, new_c])
         clear_text += ' '
         cypher_text += ' '
@@ -61,6 +62,6 @@ def rot_encode(words, rot):
 
 
 if __name__ == '__main__':
-    args = docopt(__doc__, version='Caesarian %s:%s'%(VERSION, DATE))
+    args = docopt(__doc__, version='%s %s:%s'%(TITLE, VERSION, DATE))
     args['--rot'] = int(args['--rot'])
-    main(args)
+    caesarian(args)
